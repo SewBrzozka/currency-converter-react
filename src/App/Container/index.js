3135d58d63d81d357542bb6d8163970b30305145
Seweryn Brzozowski <sew.dev.b@gmail.com>
2022-01-11 14:36:56 +0100

    components container and Header added

---
+++ src/App/Container/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+export const Container = (props) => (
+    <main className="container">
+        {props.children}
+    </main>
+);
---
+++ src/App/Header/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+
+export const Header = () => (
+    <header class="header">
+        <h1>KANTOR WALUT</h1>
+    </header>
+);
